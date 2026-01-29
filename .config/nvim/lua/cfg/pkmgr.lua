29d80968b5476acc1e69a9847ab1adb5a6a46069
aefba <shichuanyim@gmail.com>
2026-01-29 19:56:42 +0800

    add fzf

--- .config/nvim/lua/cfg/pkmgr.lua
+++ .config/nvim/lua/cfg/pkmgr.lua
@@ -18,5 +18,7 @@ vim.pack.add({
   { src = 'https://github.com/L3MON4D3/LuaSnip' },
   { src = 'https://github.com/nvim-lualine/lualine.nvim' },
   { src = 'https://github.com/nvim-mini/mini.nvim' },
+  { src = 'https://github.com/junegunn/fzf' },
+  { src = 'https://github.com/junegunn/fzf.vim' },
 
 })
